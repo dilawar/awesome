b71055d597c75bb96cdddb15998ad3365b0ccb22
Dilawar <dilawar.s.rajput@gmail.com>
2013-09-04 11:54:43 +0530

    updated lock

--- rc.lua
+++ rc.lua
@@ -476,7 +476,9 @@ clientkeys = awful.util.table.join(
         awful.key({"Mod1", "Control" }, "m", function () awful.util.spawn("amixer -q sset Master 2dB-") end),
         awful.key({"Mod1", "Control" }, "l", function () awful.util.spawn("amixer -q sset Master 2dB+") end),
         awful.key({"Mod1", "Control" }, "d", function () awful.util.spawn_with_shell("~/Scripts/manage_mpc.sh -d") end),
-        awful.key({"Mod1", "Control" }, "a", function () awful.util.spawn_with_shell("~/Scripts/manage_mpc.sh -a") end)
+        awful.key({"Mod1", "Control" }, "a", function () awful.util.spawn_with_shell("~/Scripts/manage_mpc.sh -a") end),
+	-- locking 
+	awful.key({modkey, "Control" }, "l", function () awful.util.spawn("xlock") end)
         )
 
 -- Compute the maximum number of digit we need, limited to 9
