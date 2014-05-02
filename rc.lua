02bff224d29aba3ac7801ef8750ed731b4c3165b
Dilawar <dilawars@iitb.ac.in>
2014-05-02 14:56:09 +0530

    slock is back baby

--- rc.lua
+++ rc.lua
@@ -492,8 +492,7 @@ clientkeys = awful.util.table.join(
 	    awful.key({"Mod4", "Control" }, "n", function () awful.util.spawn("rhythmbox-client --next") end),
         awful.key({"Mod4", "Control" }, "p", function () awful.util.spawn("rhythmbox-client --previous") end),
         awful.key({"Mod4", "Control" }, "t", function () awful.util.spawn("rhythmbox-client --play-pause") end),
-        --aweful.key({}, "F10", raise_conky, lower_conky),
-        --awful.key({modkey }, "F12", function () awful.util.spawn("slock") end)
+        awful.key({modkey }, "F12", function () awful.util.spawn("slock") end)
         )
 
 -- Compute the maximum number of digit we need, limited to 9
